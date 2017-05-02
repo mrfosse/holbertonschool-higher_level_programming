28cdf221c563f91e928510b8f755fbdbcac94a25
mrfosse <mrfosse99@gmail.com>
2017-05-01 21:04:14 -0700

    fixed typo.

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Holberton School"
-print("{}{}{}".format(str,str,str))
-print("{:.9}".format(str))
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
