0959af14af35170921320cf136524d22925d639a
mrfosse <mrfosse99@gmail.com>
2017-05-02 23:31:14 -0700

    pep8 fix.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,9 @@ def fizzbuzz():
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end="")
         elif num % 5 == 0:
-             print("Buzz ", end="")
+            print("Buzz ", end="")
         elif num % 3 == 0:
-              print("Fizz ", end="")
+            print("Fizz ", end="")
         else:
-               print("{} ".format(num), end="")
+            print("{} ".format(num), end="")
         num = num + 1
