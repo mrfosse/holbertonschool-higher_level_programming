50f0b09a513d97fd337efb7aa7437bb1dc8f496e
mrfosse <mrfosse99@gmail.com>
2017-05-02 23:29:50 -0700

    adding 12.

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 5 == 0:
+             print("Buzz ", end="")
+        elif num % 3 == 0:
+              print("Fizz ", end="")
+        else:
+               print("{} ".format(num), end="")
+        num = num + 1
