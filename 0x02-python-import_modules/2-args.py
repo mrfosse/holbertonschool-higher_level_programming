0d96c369c38586959a3a9f2a65fb4b568c7438e9
mrfosse <mrfosse99@gmail.com>
2017-05-04 19:59:19 -0700

    adding 2.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv) - 1
+    if num < 2:
+        print("{:d} argument".format(num))
+    else:
+        print("{:d} arguments".format(num))
+    if num > 0:
+        i = 1
+        while i <= num:
+            print("{:d}: {}".format(i, sys.argv[i]))
+            i = i + 1
