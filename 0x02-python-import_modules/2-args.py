906cc6d509e3166b160e47af0b4612c83216078c
mrfosse <mrfosse99@gmail.com>
2017-05-04 20:03:44 -0700

    fixed format issue.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 if __name__ == "__main__":
     import sys
     num = len(sys.argv) - 1
-    if num < 2:
-        print("{:d} argument".format(num))
+    if num < 1:
+        print("{:d} argument.".format(num))
+    elif num == 1:
+        print("{:d} argument:".format(num))
     else:
-        print("{:d} arguments".format(num))
+        print("{:d} arguments:".format(num))
     if num > 0:
         i = 1
         while i <= num:
