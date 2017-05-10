11db8747929c52e1329306401c1b6e13f5186602
mrfosse <mrfosse99@gmail.com>
2017-05-09 22:01:16 -0700

    adding 9.

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return (None)
+    l = len(my_list) - 1
+    i = 0
+    top = my_list[i]
+    while i <=  l:
+        if my_list[i] > top:
+            top = my_list[i]
+        i = i + 1
+    return (top)
