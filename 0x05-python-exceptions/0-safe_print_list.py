250537cd1bf9e4e4b50d33f37c5dab722501d664
mrfosse <mrfosse99@gmail.com>
2017-05-12 16:00:43 -0700

    added edge case.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None or my_list == []:
+        return (0)
     try:
         for count in range(x):
             print("{}".format(my_list[count]), end="")
