dcc84faa87c2da454fd61a7cd37dda889c73ee88
mrfosse <mrfosse99@gmail.com>
2017-05-12 16:02:49 -0700

    fixed edge case.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list is None or my_list == []:
+    if x == 0 or my_list is None or my_list == []:
         print()
         return (0)
     try:
