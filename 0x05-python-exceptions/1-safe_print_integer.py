6faf6b87f5cb55efb6a6d6e029549b6c10cda73c
mrfosse <mrfosse99@gmail.com>
2017-05-12 16:20:54 -0700

    fixed typo.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value is None:
-        print()
-        return (0)
     try:
         print("{:d}".format(value), end="")
         print()
