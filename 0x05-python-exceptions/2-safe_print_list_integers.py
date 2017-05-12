1c56cc553475decbea848066ee4910fcce6103cc
mrfosse <mrfosse99@gmail.com>
2017-05-12 16:59:28 -0700

    added error type to except.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[count]), end="")
             i = i + 1
-        except:
+        except (TypeError, ValueError):
             continue
     print()
     return (i)
