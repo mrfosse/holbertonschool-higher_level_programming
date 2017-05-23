5b91bc35a6e36dacb68a5c6403a920a75a16f322
mrfosse <mrfosse99@gmail.com>
2017-05-22 20:59:58 -0700

    adding 0 through 6 and Readme.

---
+++ 0x07-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/bin/usr/python3
+class Square:
+
+    def __init__(self, size=0, position=(0, 0)):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) and value[0] >= 0 and value[1] >= 0:
+                self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return int(self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__position[1]):
+                print(" ")
+            for i in range(0, self.__size):
+                for i in range(0, self.__position[0]):
+                        print(" ", end="")
+                for j in range(0, self.__size):
+                    print('#', end="")
+                print()
