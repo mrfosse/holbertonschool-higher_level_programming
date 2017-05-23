e298cfef87bf895b583b58235191962174b22faf
mrfosse <mrfosse99@gmail.com>
2017-05-22 22:00:59 -0700

    removed redundent code/checkers.

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -2,13 +2,7 @@
 class Square:
 
     def __init__(self, size=0):
-        if isinstance(size, int):
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
+        self.__size = size
 
     @property
     def size(self):
--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -2,13 +2,7 @@
 class Square:
 
     def __init__(self, size=0, position=(0, 0)):
-        if isinstance(size, int):
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
+        self.__size = size
         self.__position = position
 
     @property
